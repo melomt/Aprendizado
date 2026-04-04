6646568f2de9d4c7868026963cb1b114ced7a908
matheus.melo21 <matheus.melo2@aluno.ufop.edu.br>
2026-04-04 12:41:16 -0300

    pasta da IC 3

---
+++ codigos/Projeto_pandas_vinho/pandasV1wine.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+
+vinho = pd.read_csv('/home/matheusjm/Documentos/github/mycodes/Aprendizado/aprendizadodemaquina/DIDADICATECH/wine_dataset.csv')
+
+print(vinho.head())
+
+print(vinho.info)
